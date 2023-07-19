5857be88bb5720efd70e11d261dc3cf39f5ad916
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-20 00:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-	return sampleObject.hasKey(key);
+	return sampleObject.hasOwnProperty(key);
+	
 
 
 	
